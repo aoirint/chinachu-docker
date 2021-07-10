163d0c301e931340daa8f5d5b79219f6e831efcc
aoirint <aoirint@gmail.com>
2021-07-10 11:27:56 +0900

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+rm -f /var/run/chinachu-operator.pid > /dev/null 2>&1
+rm -f /var/run/chinachu-wui.pid > /dev/null 2>&1
+
 ./chinachu-operator.sh start
 ./chinachu-wui.sh start
 
-# tail -f /usr/local/chinachu/log/wui &
-# tail -f /usr/local/chinachu/log/operator &
+tail -f /usr/local/chinachu/log/wui &
+tail -f /usr/local/chinachu/log/operator &
 
 exec "$@"
